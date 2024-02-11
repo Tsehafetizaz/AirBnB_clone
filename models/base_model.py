d9275e30c24596320e332beda74d3065be2c1bbd
Tsehafetizaz <wendetefe@gmail.com>
2024-02-11 02:59:27 -0800

    hargesa

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
-"""BaseModel class module."""
-import uuid
+from uuid import uuid4
 from datetime import datetime
-from models import storage
-
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes."""
+    """Defines the BaseModel class."""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes the BaseModel instance."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key in ('created_at', 'updated_at'):
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+    def __init__(self):
+        """Initialization of the base model."""
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the BaseModel class."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates 'updated_at' and saves to file storage."""
+        """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys instance's __dict__."""
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        """Returns a dictionary containing all keys/values of the instance."""
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        return model_dict
