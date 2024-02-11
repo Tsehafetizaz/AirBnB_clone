5ca80d8b57015bdf1b37d0c8e9044168de0ef255
Tsehafetizaz <wendetefe@gmail.com>
2024-02-11 05:03:07 -0800

    jason

--- models/user.py
+++ models/user.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module for User class."""
+"""
+Module for User class.
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represents a User with inheritence from BaseModel."""
+    """
+    Represents a User with inheritence from BaseModel.
+    """
     email = ""
     password = ""
     first_name = ""
