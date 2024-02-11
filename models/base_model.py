aad9822eceb222aee0fdaa187c3a8d7f69ecabd7
Tsehafetizaz <wendetefe@gmail.com>
2024-02-11 03:59:11 -0800

    tesfa

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,48 @@
-#!/usr/bin/env python3
-"""BaseModel class for Flask applications."""
-import uuid
-from datetime import datetime
+#!/usr/bin/python3
 from uuid import uuid4
-
+from datetime import datetime
 
 class BaseModel:
-    """Base class for all models."""
+    """Defines all common attributes/methods for other classes."""
 
-    def __init__(self):
-        """Initializes the BaseModel instance."""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new BaseModel instance.
+        If kwargs is not empty, sets each key-value pair in kwargs as
+        attribute-value pairs on the instance, except for '__class__'.
+        Otherwise, assigns a new uuid and the current datetime to id and
+        created_at/updated_at attributes respectively.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a string representation of the instance."""
-        return (f"[{self.__class__.__name__}: {self.id}] - "
-                f"{self.created_at:%Y-%m-%d %H:%M:%S}")
+        """
+        Returns a string representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
+        """
+        Updates the 'updated_at' attribute with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary representation of the instance."""
+        """
+        Returns a dictionary containing all keys/values of the instance.
+        The dictionary includes the class name under the key '__class__'.
+        'created_at' and 'updated_at' are converted to string objects in ISO format.
+        """
         model_dict = self.__dict__.copy()
         model_dict['__class__'] = self.__class__.__name__
         model_dict['created_at'] = self.created_at.isoformat()
         model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
-
-
-if __name__ == "__main__":
-    # You can include code here to test the BaseModel class, but make sure
-    # to remove it when using this class in an actual Flask application.
-    pass
