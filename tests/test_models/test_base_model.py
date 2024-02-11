a7b870fbccb7e7e5770158d14b1cb729dd29b209
Tsehafetizaz <wendetefe@gmail.com>
2024-02-11 03:38:03 -0800

    doctr

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
-"""Unit tests for the BaseModel class."""
-
 import unittest
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """Defines test cases for the BaseModel class functionality."""
+    """Unit tests for the BaseModel class."""
 
-    def test_init(self):
-        """Test case for correct initialization of BaseModel instances."""
-        model = BaseModel()
-        self.assertIsInstance(model, BaseModel)
-        self.assertIsInstance(model.id, str)
-        self.assertTrue(hasattr(model, "created_at"))
-        self.assertTrue(hasattr(model, "updated_at"))
+    def test_base_model_instance(self):
+        """Test creation of a BaseModel instance."""
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
 
-    def test_str(self):
-        """Test case for the __str__ method of BaseModel."""
-        model = BaseModel()
-        expected_str_format = f"[BaseModel] ({model.id}) {model.__dict__}"
-        self.assertEqual(model.__str__(), expected_str_format)
+    def test_base_model_attributes(self):
+        """Test BaseModel attributes."""
+        my_model = BaseModel()
+        self.assertTrue(hasattr(my_model, "id"))
+        self.assertTrue(hasattr(my_model, "created_at"))
+        self.assertTrue(hasattr(my_model, "updated_at"))
 
-    def test_save(self):
-        """Test case for the 'save' method of BaseModel."""
-        model = BaseModel()
-        original_updated_at = model.updated_at
-        model.save()
-        self.assertNotEqual(model.updated_at, original_updated_at)
+    def test_str_method(self):
+        """Test the __str__ method of BaseModel."""
+        my_model = BaseModel()
+        expected_str = f"[BaseModel] ({my_model.id}) {my_model.__dict__}"
+        self.assertEqual(str(my_model), expected_str)
 
-    def test_to_dict(self):
-        """Test case for the 'to_dict' method of BaseModel."""
-        model = BaseModel()
-        model_dict = model.to_dict()
-        self.assertIsInstance(model_dict, dict)
-        self.assertEqual(model_dict['__class__'], 'BaseModel')
-        self.assertIsInstance(model_dict['created_at'], str)
-        self.assertIsInstance(model_dict['updated_at'], str)
-        self.assertIn('id', model_dict)
+    def test_save_method(self):
+        """Test the save method of BaseModel."""
+        my_model = BaseModel()
+        old_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(old_updated_at, my_model.updated_at)
+
+    def test_to_dict_method(self):
+        """Test the to_dict method of BaseModel."""
+        my_model = BaseModel()
+        self.assertIsInstance(my_model.to_dict(), dict)
 
 
 if __name__ == "__main__":
