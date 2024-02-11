d8e6549f548596df2e7ca406bc879d4ef2f8c647
Tsehafetizaz <wendetefe@gmail.com>
2024-02-11 03:16:45 -0800

    gazebo

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,1 @@
-#!/usr/bin/python3
-"""Creates a unique FileStorage instance for the application."""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
 
-classes = {
-    "BaseModel": BaseModel,
-    "User": User
-}
-storage = FileStorage()
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,37 @@
-#!/usr/bin/python3
-from uuid import uuid4
+#!/usr/bin/env python3
+"""BaseModel class for Flask applications."""
+import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    """Defines the BaseModel class."""
+    """Base class for all models."""
 
     def __init__(self):
-        """Initialization of the base model."""
+        """Initializes the BaseModel instance."""
         self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Returns a string representation of the instance."""
+        return (f"[{self.__class__.__name__}: {self.id}] - "
+                f"{self.created_at:%Y-%m-%d %H:%M:%S}")
 
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of the instance."""
+        """Returns a dictionary representation of the instance."""
         model_dict = self.__dict__.copy()
         model_dict['__class__'] = self.__class__.__name__
         model_dict['created_at'] = self.created_at.isoformat()
         model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
+
+
+if __name__ == "__main__":
+    # You can include code here to test the BaseModel class, but make sure
+    # to remove it when using this class in an actual Flask application.
+    pass
