b2cc8d3863ba45f444679febaaa967b9a5b263a2
Tsehafetizaz <wendetefe@gmail.com>
2024-03-20 12:52:00 -0700

    hha

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
-"""__init__ magic method for models directory"""
+from os import getenv
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
-
-storage = FileStorage()
+storage = None
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
+``
