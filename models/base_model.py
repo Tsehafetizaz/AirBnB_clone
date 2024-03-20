729e9e4d8002a18fa12cd2ad3df786b0a6dd55b3
Tsehafetizaz <wendetefe@gmail.com>
2024-03-20 12:18:57 -0700

    dinner

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,21 @@
-#!/usr/bin/python3
-"""BaseModel for HBnB project."""
-
-from uuid import uuid4
+# models/base_model.py
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-import models
 
+Base = declarative_base()
 
 class BaseModel:
-    """Defines the base model for all HBnB objects."""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes a new instance.
-
-        Args:
-            *args: Unused.
-            **kwargs: Key-value pairs for initializing attributes.
-        """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key in ["created_at", "updated_at"]:
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
-        else:
-            models.storage.new(self)
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def save(self):
-        """Updates `updated_at` with the current datetime."""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-        """Converts instance to a dictionary format, including class name."""
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
-        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
-        return obj_dict
-
-    def __str__(self):
-        """String representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+    def delete(self):
+        models.storage.delete(self)
+    
+    # Other methods...
