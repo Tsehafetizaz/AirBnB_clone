30fc1008e8a1e93b1dd894d2aa10041d29681b49
Tsehafetizaz <wendetefe@gmail.com>
2024-04-21 11:42:30 -0700

    zinar

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,53 +1,36 @@
 #!/usr/bin/python3
-"""start web application"""
-from flask import Flask
+"""
+starts a Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Displays 'Hello HBNB!' message for the root path (/)."""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB' message for the /hbnb path."""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_route(text):
-    """Displays 'C ' followed by the value of the text variable,
-    replacing underscores (_) with spaces.
-
-    Args:
-        text (str): The text variable passed in the URL.
-
-    Returns:
-        str: The formatted string "C " followed by the text with spaces
-              replacing underscores.
-    """
-    return f"C {text.replace('_', ' ')}"
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/python/<text:text>", strict_slashes=False)
-def python_route(text="is cool"):
-    """Displays 'Python ' followed by the value of the text variable,
-    replacing underscores (_) with spaces.
-
-    Args:
-        text (str, optional): text variable passed URL. Defaults to "is cool".
-
-    Returns:
-        str: The formatted string "Python " followed by the text with spaces
-              replacing underscores.
-    """
-    # Wrap the long line within parentheses for continuation
-    return (f"Python {text.replace('_', ' ')}")  # Remove trailing whitespace
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
-    """main fucntion"""
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
